25b5e82a709643489f2c35a896c818d7762de82f
BriannaD23 <98433092+BriannaD23@users.noreply.github.com>
2023-12-25 20:06:37 -0800

    fixing the responsiveness of the feature section

--- js/shopping-cart.js
+++
@@ -1,5 +0,0 @@
-let productsInCart = [];
-const shoppingCart = document.querySelector(".nav-link-cart");
-const navLikes = document.querySelector(".nav-link-heart");
-const customerLikes = document.querySelector("#likes");
-const products = document.querySelectorAll(".sneaker-img");
